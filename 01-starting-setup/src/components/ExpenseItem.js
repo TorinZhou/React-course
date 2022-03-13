bcda64bc1034d1e509fda234118cf1d55b0845ff
Torin Zhou <torinzhou@gmail.com>
2022-03-14 06:59:10 +0800

    3.14

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpanseItem() {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 299;
+function ExpanseItem(props) {
+  // const expenseDate = new Date();
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 299;
 
   return (
     // One root element
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
