bc79d7bdc1a5cadd194af56ef0fafcd114211893
Torin Zhou <torinzhou@gmail.com>
2022-03-15 04:35:57 +0800

    3.15

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpanseItem(props) {
-  // const expenseDate = new Date();
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 299;
-
   return (
     // One root element
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
