a21718ad69633f47645b426b26a8be5b728f579e
Torin Zhou <torinzhou@gmail.com>
2022-03-15 06:17:30 +0800

    3.15

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -3,29 +3,15 @@ import "./Expenses.css";
 const Expenses = function (props) {
   return (
     <div className="expenses">
-      {/* {props.reduce((acc, cur)=>{
-
-      },<></>)} */}
-      <ExpenseItem
-        title={props.info[0].title}
-        amount={props.info[0].amount}
-        date={props.info[0].date}
-      />
-      <ExpenseItem
-        title={props.info[1].title}
-        amount={props.info[1].amount}
-        date={props.info[1].date}
-      />
-      <ExpenseItem
-        title={props.info[2].title}
-        amount={props.info[2].amount}
-        date={props.info[2].date}
-      />
-      <ExpenseItem
-        title={props.info[3].title}
-        amount={props.info[3].amount}
-        date={props.info[3].date}
-      />
+      {props.info.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
     </div>
   );
 };
