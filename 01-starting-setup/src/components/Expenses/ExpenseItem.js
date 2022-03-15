037db61ae1a44b9e32bebd821390dd12838f0536
Torin Zhou <torinzhou@gmail.com>
2022-03-16 06:56:18 +0800

    3.16 lec49 setState

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpanseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // One of React Hook, useState always return a array with two el
+
   const clickHandler = () => {
-    console.log("clicked");
+    setTitle("Updated!");
+    console.log(title);
   };
+
   return (
     // One root element
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
