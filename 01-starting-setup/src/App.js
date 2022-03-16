220de192a82e0b2574bad2dbd734c4ed640a8113
Torin Zhou <torinzhou@gmail.com>
2022-03-16 22:04:18 +0800

    3.16

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -24,7 +24,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!🎈🎈🎈</h2>
+      <NewExpense />
       <Expenses info={expenses} />
     </div>
   );
