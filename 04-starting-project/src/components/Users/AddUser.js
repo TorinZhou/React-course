0104c3db81f00e81c6b732e7307b0f392092fde5
Torin Zhou <torinzhou@gmail.com>
2022-03-22 04:19:29 +0800

    3.22-lec93

--- 04-starting-project/src/components/Users/AddUser.js
+++ 04-starting-project/src/components/Users/AddUser.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUserName] = useState("");
+  const [enteredAge, setAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
     console.log("😀😀😀");
+    console.log(`${enteredUsername} : ${enteredAge}`);
   };
+
+  const inputHandler = (type) => (event) => {
+    type === "username"
+      ? setEnteredUserName(event.target.value)
+      : setAge(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form className={styles.form} onSubmit={addUserHandler}>
         <label htmlFor="username">Course Goal</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={inputHandler("username")} />
         <label htmlFor="age">Age(Year)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={inputHandler("age")} />
         <Button className="big" type="submit" onClick={addUserHandler}>
           Add User
         </Button>
