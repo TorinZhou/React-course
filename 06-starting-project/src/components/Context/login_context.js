91377b06354c196f16c704b4d42f532a7bf25187
Torin Zhou <torinzhou@gmail.com>
2022-04-03 10:33:19 +0800

    4.3

--- 06-starting-project/src/components/Context/login_context.js
+++ 06-starting-project/src/components/Context/login_context.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+
 const LoginContext = React.createContext({
   isLoggedIn: false,
+  onLogout: ()=>{},
 });
 
 export const LoginComponent = (props) => {
-  return <LoginContext>{props.content}</LoginContext>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log("Calling LoginComponent");
+  
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("storedLoginState", 1);
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("storedLoginState");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <LoginContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}>
+      {props.content}
+    </LoginContext.Provider>
+  );
 }
 
 export default LoginContext;
