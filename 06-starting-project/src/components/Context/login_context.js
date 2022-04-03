773bd4aa8d3f9823aa605e9a045220bf22515e6a
Torin Zhou <torinzhou@gmail.com>
2022-04-03 10:53:23 +0800

    4.3

--- 06-starting-project/src/components/Context/login_context.js
+++ 06-starting-project/src/components/Context/login_context.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from "react";
 const LoginContext = React.createContext({
   isLoggedIn: false,
   onLogout: ()=>{},
+  onLogin: ()=>{},
 });
 
 export const LoginComponent = (props) => {
