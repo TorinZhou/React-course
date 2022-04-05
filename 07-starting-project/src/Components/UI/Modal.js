70fef17fe0e21569e376c4ec79ab21e9a3bfee87
Torin Zhou <torinzhou@gmail.com>
2022-04-05 13:14:59 +0800

    4.5

--- 07-starting-project/src/Components/UI/Modal.js
+++ 07-starting-project/src/Components/UI/Modal.js
@@ -1,5 +1,5 @@
 import classes from "./Modal.module.css";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import { Fragment } from "react";
 
 const Backdrop = (props) => {
@@ -15,15 +15,16 @@ const ModalOverlay = (props) => {
 };
 
 const portalElement = document.getElementById("overlays");
+
 const Modal = (props) => {
   console.log("Calling Modal");
   console.log(portalElement);
   console.log(props.children);
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
-      {ReactDom.createPortal(
-        <ModalOverlay>{props.children} </ModalOverlay>,
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
     </Fragment>
