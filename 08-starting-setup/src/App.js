869675005532d17dddfd4e452c13d7ec333a3e1d
Torin Zhou <torinzhou@gmail.com>
2022-04-09 18:56:56 +0800

    4.9

--- 08-starting-setup/src/App.js
+++ 08-starting-setup/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagrahp] = useState(false);
+  const toggleParagraphHandler = () => {
+    setShowParagrahp((curState) => !curState);
+    // setShowParagrahp(true);
+  };
+  console.log("App running");
+  const removeParagraphHandler = () => {};
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>Additional paragraph</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle a paragraph</Button>
     </div>
   );
 }
