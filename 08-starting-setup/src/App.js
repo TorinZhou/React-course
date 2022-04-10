7b4396c385358117e9ead77b4eab36a43e8ea088
Torin Zhou <torinzhou@gmail.com>
2022-04-10 11:49:16 +0800

    4.10

--- 08-starting-setup/src/App.js
+++ 08-starting-setup/src/App.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagrahp] = useState(false);
-  const toggleParagraphHandler = () => {
-    setShowParagrahp((curState) => !curState);
-    // setShowParagrahp(true);
-  };
+  const [enableButton, setEnableButton] = useState(false);
   console.log("App running");
-  const removeParagraphHandler = () => {};
+  const toggleParagraphHandler = useCallback(() => {
+    if (enableButton) setShowParagrahp((curState) => !curState);
+    // setShowParagrahp(true);
+  }, [enableButton]);
+  const enableButtonHandler = useCallback(() => {
+    setEnableButton(true);
+  }, []);
+  const arr = useMemo(() => {
+    return [1, 2, 3, 4, 5];
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {showParagraph && <p>Additional paragraph</p>}
+      <Button onClick={enableButtonHandler} arr={arr}>
+        Enable the Button
+      </Button>
       <Button onClick={toggleParagraphHandler}>Toggle a paragraph</Button>
     </div>
   );
 }
 
-export default App;
+export default React.Meme(App);
