bef78b56192203a0eb1dc8f611683605ec65c890
Torin Zhou <torinzhou@gmail.com>
2022-04-11 19:09:54 +0800

    4.11

--- 10-starting-project/src/App.js
+++ 10-starting-project/src/App.js
@@ -6,28 +6,48 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fecthMovieHandler = async () => {
-    const response = await fetch(`${URL}films`);
-    const data = await response.json();
-    const transformedData = data.results.map((movie) => {
-      return {
-        title: movie.title,
-        id: movie["episode_id"],
-        releaseDate: movie["release_date"],
-        openingText: movie["opening_crawl"],
-      };
-    });
-    setMovies(transformedData);
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${URL}films/`);
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Something went wrong: ${response.status}`);
+      }
+      const data = await response.json();
+      const transformedData = data.results.map((movie) => {
+        return {
+          title: movie.title,
+          id: movie["episode_id"],
+          releaseDate: movie["release_date"],
+          openingText: movie["opening_crawl"],
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   };
-
+  let content = <p>Found no movies</p>;
+  if (isLoading) {
+    content = <p>Loading</p>;
+  }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
   return (
     <React.Fragment>
       <section>
         <button onClick={fecthMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
