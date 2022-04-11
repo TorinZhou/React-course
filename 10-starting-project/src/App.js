83ad9bac0f78423f152c026761bf098b04185e0c
Torin Zhou <torinzhou@gmail.com>
2022-04-11 16:27:00 +0800

    4.11 using async

--- 10-starting-project/src/App.js
+++ 10-starting-project/src/App.js
@@ -6,24 +6,18 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fecthMovieHandler = () => {
-    fetch(`${URL}films`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const transformedData = data.results.map((movie) => {
-          console.log("1");
-          return {
-            title: movie.title,
-            id: movie["episode_id"],
-            releaseDate: movie["release_date"],
-            openingText: movie["opening_crawl"],
-          };
-        });
-        setMovies(transformedData);
-      });
+  const fecthMovieHandler = async () => {
+    const response = await fetch(`${URL}films`);
+    const data = await response.json();
+    const transformedData = data.results.map((movie) => {
+      return {
+        title: movie.title,
+        id: movie["episode_id"],
+        releaseDate: movie["release_date"],
+        openingText: movie["opening_crawl"],
+      };
+    });
+    setMovies(transformedData);
   };
 
   return (
