73c757c87966a9b9b0162f8fb7c0b39becec6970
Torin Zhou <torinzhou@gmail.com>
2022-04-11 16:22:29 +0800

    4.11

--- 10-starting-project/src/App.js
+++ 10-starting-project/src/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import URL from "./store/config";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const fecthMovieHandler = () => {
+    fetch(`${URL}films`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const transformedData = data.results.map((movie) => {
+          console.log("1");
+          return {
+            title: movie.title,
+            id: movie["episode_id"],
+            releaseDate: movie["release_date"],
+            openingText: movie["opening_crawl"],
+          };
+        });
+        setMovies(transformedData);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fecthMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
