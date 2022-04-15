c61a429b93d93e077820f677ba27618c66e8bc8c
Torin Zhou <torinzhou@gmail.com>
2022-04-15 15:41:24 +0800

    4.15

--- 13-starting-project/src/components/SimpleInput.js
+++ 13-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enterValue, setEnterValue] = useState("Torin");
+  const inputRef = useRef();
+
+  // * Ref-based submit handler
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const data = inputRef.current.value;
+    console.log(data);
+  };
+  // * State-based key-stroke handler
+  const valueChangeHandler = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setEnterValue(value);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={inputRef}
+          value={enterValue}
+          // * defaultValue="Torin"
+          onChange={valueChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
