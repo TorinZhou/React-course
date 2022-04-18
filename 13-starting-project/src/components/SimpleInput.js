73098d01adebf8757566222ae4c229f4cbf85f33
Torin Zhou <torinzhou@gmail.com>
2022-04-19 00:17:35 +0800

    4.19

--- 13-starting-project/src/components/SimpleInput.js
+++ 13-starting-project/src/components/SimpleInput.js
@@ -1,33 +1,82 @@
 import { useState, useRef } from "react";
 
-const SimpleInput = (props) => {
-  const [enterValue, setEnterValue] = useState("Torin");
-  const inputRef = useRef();
+// * Validation Funtion
+const nameValidation = (name) => {
+  const regEx = /^\w+$/g;
+  return regEx.test(name);
+};
+const emailValidation = (email) => {
+  const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+  return regEx.test(email);
+};
 
-  // * Ref-based submit handler
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailIsTouched, setEnteredEmailIsTouched] = useState(false);
+  const nameInputIsValid = nameValidation(enteredName);
+  const emailInputIsValid = emailValidation(enteredEmail);
+  let formIsValid = false;
+  console.log(emailInputIsValid, enteredEmailIsTouched);
+  if (nameInputIsValid && emailInputIsValid) {
+    formIsValid = true;
+  }
+  const nameChangeHandler = (e) => {
+    const value = e.target.value;
+    setEnteredName(value);
+  };
+  const emailChangeHandler = (e) => {
+    const value = e.target.value;
+    setEnteredEmail(value);
+  };
   const submitHandler = (e) => {
     e.preventDefault();
-    const data = inputRef.current.value;
-    console.log(data);
+    setEnteredNameIsTouched(true);
+    setEnteredEmailIsTouched(true);
+    if (!nameInputIsValid || !emailInputIsValid) {
+      return;
+    }
+    setEnteredName("");
+    setEnteredEmail("");
+    setEnteredNameIsTouched(false);
+    setEnteredEmailIsTouched(false);
   };
-  // * State-based key-stroke handler
-  const valueChangeHandler = (e) => {
-    const value = e.target.value;
-    console.log(value);
-    setEnterValue(value);
-  };
+
+  const nameInputClassName =
+    !nameInputIsValid && enteredNameIsTouched
+      ? "form-control invalid"
+      : "form-control";
+  const emailInputClassName =
+    !emailInputIsValid && enteredEmailIsTouched
+      ? "form-control invalid"
+      : "form-control";
+
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
-        <label htmlFor="name">Your Name</label>
+      <div className={nameInputClassName}>
+        <label htmlFor="name">Your Name: </label>
         <input
           type="text"
           id="name"
-          ref={inputRef}
-          value={enterValue}
-          // * defaultValue="Torin"
-          onChange={valueChangeHandler}
+          value={enteredName}
+          onChange={nameChangeHandler}
         />
+        {!nameInputIsValid && enteredNameIsTouched && (
+          <p className="error-text">Something went wrong</p>
+        )}
+      </div>
+      <div className={emailInputClassName}>
+        <label htmlFor="email">Your Email: </label>
+        <input
+          type="text"
+          id="email"
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+        />
+        {!emailInputIsValid && enteredEmailIsTouched && (
+          <p className="error-text">Something went wrong</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
