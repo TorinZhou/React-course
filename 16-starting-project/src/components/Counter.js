c3128e5712923a529acdcf54b92ea00a5e4b0960
Torin Zhou <torinzhou@gmail.com>
2022-04-23 13:41:38 +0800

    4.23

--- 16-starting-project/src/components/Counter.js
+++ 16-starting-project/src/components/Counter.js
@@ -1,12 +1,21 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
+import store from "../store";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // ! use useSelector custom hook we don't need to subscribe or getState manually.
+  // store.subscribe(Counter);
+  // const counterValue = store.getState().counter;
 
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {
+    console.log("clicked");
+    store.dispatch({ type: "INCREMENT" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {counter} --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
