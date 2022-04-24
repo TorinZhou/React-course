2703a1ef76077f8faca50d8fcad487f037a04528
Torin Zhou <torinzhou@gmail.com>
2022-04-24 15:34:59 +0800

    4.24

---
+++ 17-Redux2/src/App.js
@@ -0,0 +1,18 @@
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+
+import { useSelector } from "react-redux";
+
+function App() {
+  const cartIsShown = useSelector((state) => state.cart.cartIsShown);
+  return (
+    <Layout>
+      {cartIsShown && <Cart />}
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
+s;
